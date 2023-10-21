f44f2968097abf86f7ed59855194f19355168a02
Maria <maria.mandrykina@gmail.com>
2023-10-21 20:05:16 +0300

    commit user_payments

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+
+from payments.models import Payments
 from users.models import User
 
 
 class UserSerializers(serializers.ModelSerializer):
+    payments = serializers.SerializerMethodField()
+
+    def get_payments(self, obj_user):
+        payments = Payments.objects.filter(user=obj_user)
+        return [(p.payment_amount, p.payment_method, p.date) for p in payments]
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'city']
+        fields = ['id', 'email', 'name', 'city', 'payments']
 
 
 
